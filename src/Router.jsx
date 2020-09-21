aa38bdd87eec832415111c5adbd4f01a716b253b
Steve Scott <sgscott113@gmail.com>
2020-09-20 21:12:50 -0400

    Correction to router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Redirect } from 'react-router-dom'
+import HowRichAmI from 'components/HowRichAmI'
 
 const Router = () => <BrowserRouter>
   <Route path='/' exact render={({ location }) => <Redirect to={{ pathname: '/embed', search: location.search }} />} />
